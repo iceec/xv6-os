0a231966568eb982289922af0fb7ddb4a8699395
iceec <iceec1733197669@163.com>
2024-07-05 23:40:04 -0700

    finish

--- kernel/proc.c
+++ kernel/proc.c
@@ -517,6 +517,7 @@ scheduler(void)
         swtch(&c->context, &p->context);
         // Process is done running for now.
         // It should have changed its p->state before coming back.
+        kvminithart();
         c->proc = 0;
         found = 1;
       }
